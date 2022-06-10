58a02245dd865acf4f0f3bfbbbe4ffb0320fbe35
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2022-06-10 11:34:22 +0600

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -14,7 +14,7 @@ async function publishContact(req,res){
     });
   
     let savedContactObj= await newContactObj.save();
-    res.jspn({message:"successs",savedObj:savedContactObj});
+    res.json({message:"successs",savedObj:savedContactObj});
   }catch(err){
     console.log(err);
     res.status(404).json({error:"error_sending"});
