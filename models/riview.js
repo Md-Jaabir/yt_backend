ac6e3e71ff9a54357806a447fe3e2f54e9e30507
Md-Jaabir <100750999+Md-Jaabir@users.noreply.github.com>
2022-06-10 09:33:04 +0600

    Update riview.js

--- models/riview.js
+++ models/riview.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 const riviewSchema= mongoose.Schema({
   name:{type:String,required:true},
   stars:{type:String,required:true},
+  email:{type:String,required:true},
   description:{type:String,required:true}
 })
 
